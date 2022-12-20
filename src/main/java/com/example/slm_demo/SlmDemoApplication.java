690612c54197ccd9179e1698c046afe392e6a73f
Florian <fgotschim@gmail.com>
2022-12-20 20:36:22 +0100

    git init

---
+++ src/main/java/com/example/slm_demo/SlmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmDemoApplication.class, args);
+    }
+
+}
